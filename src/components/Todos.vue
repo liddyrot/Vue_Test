6bd133259e03e508dc8eae2d8124b14db474f186
Liddy Rothermel <earothermel@gmail.com>
2019-05-06 19:28:44 -0500

    Began tutorial

---
+++ src/components/Todos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>
+      v-for="todo in todos"
+      <h3>{{todo.title}}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Todos",
+  props: ["todos"]
+};
+</script>
+
+<style scoped>
+</style>
+
+
